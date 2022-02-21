b4af139e479db71052fe714f00495b77bc6a5643
koodeu <limiera@gmail.com>
2022-02-21 22:05:12 +0100

    added thymeleaf

--- src/main/java/com/example/koodev/learn2spring/controller/PlayerController.java
+++ src/main/java/com/example/koodev/learn2spring/controller/PlayerController.java
@@ -3,18 +3,23 @@ package com.example.koodev.learn2spring.controller;
 import com.example.koodev.learn2spring.Player;
 import com.example.koodev.learn2spring.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping(path = "/web")
 public class PlayerController {
 
     @Autowired
     public PlayerService playerService;
 
     @GetMapping(path = "/players")
+    @ResponseBody
     public List<Player> getPlayers(){
         return playerService.getPlayerList();
     }
